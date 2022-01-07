70c97dc54667411d0eaa6bcd94a069140864c491
Alexis Massa <alexis.massa351@gmail.com>
2022-01-07 15:35:44 +0100

    Mis en place site terminé TODO : ajouter route /categorie/id + panier + recherche et langues dans navbar

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,36 @@
 //  Controller/DefaultController.php
 namespace App\Controller;
 
+use App\Service\ShopService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
-    public function hello($userName)
+    /**
+     * Home page
+     */
+    public function index()
     {
+        return $this->render('home.html.twig');
+    }
+
+    /**
+     * Shoppping page
+     */
+    public function shop(ShopService $shopService)
+    {
+        $categories = $shopService->findAllCategories();
         return $this->render(
-            'hello.html.twig',
-            ['userName' => $userName,]
+            'shop.html.twig',
+            ["categories" => $categories]
         );
+    }
+
+    /**
+     * Contact page
+     */
+    public function contact()
+    {
+        return $this->render('contact.html.twig');
     }
 }
