cf0e9ef5d72e0f9f5aebf2053551ab59026fc209
Alexis Massa <63648046+alexis-massa@users.noreply.github.com>
2022-02-12 14:39:38 +0100

    Cart to Command done (to test) TODO : Authentication

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -4,8 +4,12 @@ namespace App\Service;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use App\Controller\ShopController;
+use App\Entity\Command;
+use App\Entity\CommandLine;
 use App\Entity\Product;
-use Doctrine\ORM\EntityManager;
+use DateTime;
+use DateTimeZone;
+use Doctrine\ORM\EntityManagerInterface;
 
 // Service to manage cart in session
 class CartService
@@ -17,13 +21,32 @@ class CartService
     private $cart; // Associative array idProduct => amount
     // so $this->cart[$i] = amount of product with id = $i
     // Constructor
-    public function __construct(SessionInterface $session, ShopController $shop)
+public function __construct(SessionInterface $session, ShopController $shop, EntityManagerInterface $em)
     {
         // Get shop and session service
         $this->shop = $shop;
         $this->session = $session;
+        // Get EntityManager
+        $this->em = $em;
         // Get cart if exists, else initialize
-        $this->cart = $session->get(self::CART_SESSION, array()); // initialize cart
+        $this->cart = $this->getCart(); // initialize cart
+    }
+
+
+    /**
+     * Update $this->cart with cart in $this->session
+     */
+    public function getCart()
+    {
+        $this->cart = $this->session->get(self::CART_SESSION, array());
+    }
+
+    /**
+     * Set $this->cart in $this->session['cart']
+     */
+    public function setCart(): void
+    {
+        $this->session->set(self::CART_SESSION, $this->cart);
     }
 
     /**
@@ -31,12 +54,12 @@ class CartService
      *      array of element [ "product" => product, "amount" => amount ]
      * @return Array cart content
      */
-    public function getContent($em): array
+    public function getContent(): array
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         $content = array();
         foreach ($this->cart as $id => $amount) {
-            array_push($content, ["product" => $em->getRepository(Product::class)->find($id), "amount" => $amount]);
+            array_push($content, ["product" => $this->em->getRepository(Product::class)->find($id), "amount" => $amount]);
         }
         return $content;
     }
@@ -45,12 +68,12 @@ class CartService
      * Return the total amount of the cart
      * @return float amount
      */
-    public function getTotal($em)
+    public function getTotal()
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         $total = 0.0;
         foreach ($this->cart as $id => $amount) {
-            $total += $em->getRepository(Product::class)->find($id)->getPrice() * $amount;
+            $total += $this->em->getRepository(Product::class)->find($id)->getPrice() * $amount;
         }
         return $total;
     }
@@ -61,7 +84,7 @@ class CartService
      */
     public function getNbProducts()
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         $nb = 0;
         foreach ($this->cart as $id => $amount) {
             $nb += $amount;
@@ -76,10 +99,10 @@ class CartService
      */
     public function addProduct(int $idProduct, int $amount = 1)
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         if (array_key_exists($idProduct, $this->cart)) $this->cart[$idProduct]++;
         else $this->cart[$idProduct] = $amount;
-        $this->session->set(self::CART_SESSION, $this->cart);
+        $this->setCart();
     }
 
     /**
@@ -89,9 +112,9 @@ class CartService
      */
     public function removeProduct(int $idProduct, int $amount = 1)
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         $this->cart[$idProduct] -= $amount;
-        $this->session->set(self::CART_SESSION, $this->cart);
+        $this->setCart();
     }
 
     /**
@@ -100,9 +123,9 @@ class CartService
      */
     public function deleteProduct(int $idProduct)
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         unset($this->cart[$idProduct]);
-        $this->session->set(self::CART_SESSION, $this->cart);
+        $this->setCart();
     }
 
     /**
@@ -110,8 +133,31 @@ class CartService
      */
     public function empty()
     {
-        $this->cart = $this->session->get(self::CART_SESSION, array());
+        $this->getCart();
         $this->cart = array();
-        $this->session->set(self::CART_SESSION, $this->cart);
+        $this->setCart();
+    }
+
+
+    /**
+     * Convert cart to a new Command
+     *
+     * @param  int $idUser Cart owner's id
+     * @return Command|null The newly created command or null is $this->cart is empty
+     */
+    public function cartToCommand($idUser): ?Command
+    {
+        $this->getCart(); //Update cart content
+        
+        if (empty($this->cart)) return null; // abort if cart is empty
+
+        $command = new Command();
+        $command->setDate(new DateTime('now', new DateTimeZone('Europe/Paris')))->setUser($this->em->getRepository(User::class)->find($idUser)); // Add datetime and user to command
+        foreach ($this->cart as $id => $amount) { // add all cart items as commandlines
+            $cl = new CommandLine();
+            $cl->setProduct($this->em->getRepository(Product::class)->find($id))->setAmount($amount);
+            $command->addCommandLine($cl);
+        }
+        return $command;
     }
 }
